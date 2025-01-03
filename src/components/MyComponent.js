fce028a5e2cc8c9d9f26a03e2b52956e4d416af3
ngdi2006 <ngdi2006@gmail.com>
2025-01-03 22:28:18 +0900

    arrow funtion

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -12,17 +12,37 @@ class MyComponent extends react.Component {
     };
 
 
+handleClick (event) {
+        console.log("click me vào tôi");
+        // console.log(event.target)      
+        // console.log(event)        
+        console.log("tên :", this.state.name );
+        // console.log("tuổi", this.state.age);
+        
+
+        this.setState({
+            name: 'link',
+            age: Math.random()
+        })
+        
+        
+        
+}
+handleOnMouseOver = (event) => { 
+    // console.log(event.pageX);
+    
+}
+
 
     //JSX
     render() {
         return (
 
-            <div>
-
-                đây là thư mục đầu tiên của component 
-                <hr></hr>
-               
-                Tên {this.state.name} and địa chỉ {this.state.address}
+            <div>               
+                Tên {this.state.name} and địa chỉ {this.state.address} 
+                age {this.state.age}
+                <button onClick={(event) => {this.handleClick(event)}}>click </button>
+                <button onMouseOver={this.handleOnMouseOver}>hover me</button>
             </div>
         )
     }
